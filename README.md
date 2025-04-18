0ec9672d35bf7f1407723d848618b4ee7349ace4
STSchiff <6112968+chrisheib@users.noreply.github.com>
2025-04-18 23:24:35 +0200

    upgrade dependencies

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ flutter install apk
 flutter emulators
 flutter emulators --launch Pixel_8_Pro_API_34
 flutter clean; flutter build apk --release; flutter install apk --release
+
+flutter pub outdated # check dependencies
+flutter pub upgrade # upgrade dependencies
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import audio_session
 import connectivity_plus
 import just_audio
 import path_provider_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
